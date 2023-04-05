ff493bfa81231537743bf74e375d1ce5d4c6bacd
develcool777 <develcool777@users.noreply.github.com>
2023-04-05 09:13:51 +0000

    deploy: ecf358a47fd217ee021263e102f48b79195e765e

---
+++ assets/Play-9ceb66df.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as s,g as o}from"./index-2fa162e9.js";const c={},n={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",viewBox:"0 0 24 24"},_=o("path",{d:"M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-3 18v-12l10 6-10 6z"},null,-1),a=[_];function r(d,h){return t(),s("svg",n,a)}const l=e(c,[["render",r]]);export{l as default};
